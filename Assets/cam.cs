add8a7ab35cf647cb5167e85ae2ee865dd2dad88
Akshit Arora <akshit.arora@colorado.edu>
2018-09-07 22:27:23 -0600

    till_step_5

---
+++ Assets/cam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cam : MonoBehaviour {
+
+	public BallPrefab ball_prefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			BallPrefab ball = Instantiate<BallPrefab>(ball_prefab);
+			ball.transform.localPosition = transform.position;
+			ball.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * Random.Range(750,900));
+		}
+	}
+}
